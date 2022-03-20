806ae78e67860015429e168155193f56ba3f55c6
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-03-20 18:10:37 +0900

    add daisy-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,10 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        bgGoogle: '#1a1a1d',
-        bgText: '#989A9E',
-        yellow: '#9b9700',
-        red: '#713a3c',
-        white: '#e7eae8',
-        green: '#395142',
-        grays: '#454552',
-      },
-    },
+    extend: {},
   },
   variants: {
     backgroundColor: ['responsive', 'focus', 'active', 'hover'],
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
